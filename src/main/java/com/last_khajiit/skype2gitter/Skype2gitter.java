72456d9e901b418c6823e95725c09b9bbccd410d
last-khajiit <last.khajiit@gmail.com>
2016-06-02 17:31:08 +0300

    Add Gitter messages posting functionality

--- src/main/java/com/last_khajiit/skype2gitter/Skype2gitter.java
+++ src/main/java/com/last_khajiit/skype2gitter/Skype2gitter.java
@@ -8,20 +8,33 @@ import com.samczsun.skype4j.events.chat.message.MessageReceivedEvent;
 import com.samczsun.skype4j.exceptions.ConnectionException;
 import com.samczsun.skype4j.exceptions.NotParticipatingException;
 import com.samczsun.skype4j.exceptions.InvalidCredentialsException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class Skype2gitter{		
 	public static void main(String[] args){
 		final String username = args[0];
 		final String password = args[1];
+		final String gittetChatId = args[2];
+		final String gitterToken = args[3];		
+		/*
+		final String gitterChatName = args[3]; //TODO implement searaching gitter-chats
+		final String skypeChat = ""; //TODO implement searching skype-chats
+		*/
 		try {
 			Skype skype = new SkypeBuilder(username, password).withAllResources().build();
 			skype.login();
 			System.out.println("Logged in");
 			skype.getEventDispatcher().registerListener(new Listener() {
 			  @EventHandler
-			  public void onMessage(MessageReceivedEvent e) {
+			  public void onMessage(MessageReceivedEvent e){
 				try {
-					System.out.println("Got message: " + e.getMessage().getContent() + " sent by " + e.getMessage().getSender().getDisplayName());
+					String message = formatSkypeMessage(e.getMessage().getSender().getDisplayName(), 
+							e.getMessage().getContent().toString());
+					System.out.println(message);
+					sendGitterMessage(gitterToken, gittetChatId, message);
 				}catch(ConnectionException ex) {
 					System.out.println(ex.getMessage());
 				}
@@ -31,5 +44,36 @@ public class Skype2gitter{
 		}catch(ConnectionException | NotParticipatingException | InvalidCredentialsException e) {
             System.out.println(e.getMessage());
         }
+	}
+	
+	private static void sendGitterMessage(String gitterToken, String gittetChatId, String message){
+		try {
+			String url = "https://api.gitter.im/v1/rooms/"+gittetChatId+"/chatMessages";
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			con.setRequestProperty("Accept", "application/json");
+			con.setRequestProperty("Authorization", "Bearer "+gitterToken);
+			con.setDoOutput(true);
+			DataOutputStream os = new DataOutputStream(con.getOutputStream());
+			os.write(message.getBytes("UTF-8"));
+			os.flush();
+			os.close();
+			int responseCode = con.getResponseCode();
+			System.out.println("Response Code : " + responseCode);
+		}catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	private static String formatSkypeMessage(String senderName, String message){
+		String formatedMessage = "{\"text\":\"" + senderName.replace("\"","\\\"") 
+				+ " says:\\n>" + message.replace("\"","\\\"") + "\"}";
+		formatedMessage = formatedMessage.replace("<b>","**").replace("</b>","**").replace("<i>","*")
+				.replace("</i>","*").replace("<s>","~~").replace("</s>","~~").replace("<pre>","```")
+				.replace("</pre>","```").replace("\n", " ").replace("\r", " ");	
+		//message.replace("</a>","```");		
+		return formatedMessage;
 	}
 }
